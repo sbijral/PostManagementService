e4a9da4f4fd2ccefb52b91ea5534ee528c7e3578
Ravi Maurya <mnnitianravimaurya@gmail.com>
2019-10-03 18:47:34 +0530

    updated PostController

--- src/main/java/com/yolo/PostManagementService/Controllers/PostController.java
+++ src/main/java/com/yolo/PostManagementService/Controllers/PostController.java
@@ -1,5 +1,4 @@
 package com.yolo.PostManagementService.Controllers;
-
 import com.yolo.PostManagementService.Services.MongoPostService;
 import com.yolo.PostManagementService.Services.SolrService;
 import com.yolo.PostManagementService.enums.PostStatus;
@@ -9,7 +8,6 @@ import com.yolo.PostManagementService.resources.Comment;
 import com.yolo.PostManagementService.resources.Post;
 import com.yolo.PostManagementService.resources.PostVo;
 import com.yolo.PostManagementService.utility.ApiResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.http.HttpStatus;
@@ -19,28 +17,23 @@ import reactor.core.CoreSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
-
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-
 @RestController
 public class PostController {
+	@Autowired
+	private PostRepository postRepository;
 
-    @Autowired
-    private PostRepository postRepository;
-    
-    @Autowired
-    private CommentRepository commentRepository;
-
-    @Autowired
-    private MongoPostService mongoPostService;
-    @Autowired
-    SolrService solrService;
-
-    @PostMapping("/addPost")
-    public DeferredResult<ApiResponse<Post>> savePost(@RequestBody Post postBody)
+	@Autowired
+	private CommentRepository commentRepository;
+	@Autowired
+	private MongoPostService mongoPostService;
+	@Autowired
+	SolrService solrService;
+	@PostMapping("/addPost")
+	public DeferredResult<ApiResponse<Post>> savePost(@RequestBody Post postBody)
 	{
-    	boolean moderationFlag=false;
+		boolean moderationFlag=false;
 		postBody.setPostId(UUID.randomUUID().toString());
 		ApiResponse<Post> result = new ApiResponse<Post>();
 		DeferredResult<ApiResponse<Post>> defResult = new DeferredResult<>();
@@ -48,75 +41,67 @@ public class PostController {
 		if (moderationFlag)
 		{
 			postBody.setStatus(PostStatus.PENDING_APPROVAL);
-			}
-		
+		}
+
 		else
 		{
 			postBody.setStatus(PostStatus.APPROVED);
 		}
-			
+
 		postRepository.insert(postBody).doOnError(error -> {
 			result.setStatus(HttpStatus.NOT_MODIFIED);
 			result.setMessage("Not ABle to Add the user " + error);
 			result.setDebugMessage(error.getCause().toString());
 			defResult.setResult(result);
 		}).doOnNext(body -> {
-				result.setStatus(HttpStatus.OK);
-				result.setMessage("Post Added" + body.getPostId());
-				result.setResponseObject(body);
-				defResult.setResult(result);
-				solrService.insertInSolr(postBody);
-
+			result.setStatus(HttpStatus.OK);
+			result.setMessage("Post Added" + body.getPostId());
+			result.setResponseObject(body);
+			defResult.setResult(result);
+			solrService.insertInSolr(postBody);
 		}).subscribe();
-
 		return defResult;
 	}
-    
-    @GetMapping("/post/{postId}")
-    public DeferredResult<ApiResponse<Post>> getPost(@PathVariable String postId)
-	{
 
+	@GetMapping("/post/{postId}")
+	public DeferredResult<ApiResponse<Post>> getPost(@PathVariable String postId)
+	{
 		ApiResponse<Post> result = new ApiResponse<Post>();
 		DeferredResult<ApiResponse<Post>> defResult = new DeferredResult<>();
 		postRepository.findById(postId).doOnNext(post -> {
-			result.setStatus(HttpStatus.OK);
-			result.setResponseObject(post);
-			defResult.setResult(result);
-		}
-
+					result.setStatus(HttpStatus.OK);
+					result.setResponseObject(post);
+					defResult.setResult(result);
+				}
 		).doOnError(error -> {
 			result.setStatus(HttpStatus.NOT_MODIFIED);
 			result.setMessage("Not ABle to Add the user " + error);
 			result.setDebugMessage(error.getCause().toString());
 			defResult.setResult(result);
 		}).subscribe();
-
 		return defResult;
 	}
-    
-    @DeleteMapping("/post/{postId}")
-    public DeferredResult<ApiResponse<Post>> deletePost(@PathVariable String postId)
-    {
-    ApiResponse<Post> result = new ApiResponse<Post>();
-	DeferredResult<ApiResponse<Post>> defResult = new DeferredResult<>();
-	postRepository.deleteById(postId).doOnNext(post -> {
-		result.setStatus(HttpStatus.OK);
-		defResult.setResult(result);
+
+	@DeleteMapping("/post/{postId}")
+	public DeferredResult<ApiResponse<Post>> deletePost(@PathVariable String postId)
+	{
+		ApiResponse<Post> result = new ApiResponse<Post>();
+		DeferredResult<ApiResponse<Post>> defResult = new DeferredResult<>();
+		postRepository.deleteById(postId).doOnNext(post -> {
+					result.setStatus(HttpStatus.OK);
+					defResult.setResult(result);
+				}
+		).doOnError(error -> {
+			result.setStatus(HttpStatus.NOT_MODIFIED);
+			result.setMessage("Not ABle to Delete the user " + error);
+			result.setDebugMessage(error.getCause().toString());
+			defResult.setResult(result);
+		}).subscribe();
+		return defResult;
 	}
 
-	).doOnError(error -> {
-		result.setStatus(HttpStatus.NOT_MODIFIED);
-		result.setMessage("Not ABle to Delete the user " + error);
-		result.setDebugMessage(error.getCause().toString());
-		defResult.setResult(result);
-	}).subscribe();
-
-	return defResult;
-}
-    
-    @PostMapping("/post/{postId}/addComment")
-    public DeferredResult<ApiResponse<Comment>> addComment(@PathVariable String postId, @RequestBody Comment commentBody)
-
+	@PostMapping("/post/{postId}/addComment")
+	public DeferredResult<ApiResponse<Comment>> addComment(@PathVariable String postId, @RequestBody Comment commentBody)
 	{
 		commentBody.setCommentId(UUID.randomUUID().toString());
 		ApiResponse<Comment> result = new ApiResponse<Comment>();
@@ -133,215 +118,180 @@ public class PostController {
 			result.setDebugMessage(error.getCause().toString());
 			defResult.setResult(result);
 		}).subscribe();
-
 		return defResult;
-
 	}
-    
-    @PutMapping("/post/{postId}/closePost")
-    public DeferredResult<ApiResponse<Void>> closePost(@PathVariable String postId)
-	{
 
+	@PutMapping("/post/{postId}/closePost")
+	public DeferredResult<ApiResponse<Void>> closePost(@PathVariable String postId)
+	{
 		ApiResponse<Void> result = new ApiResponse<Void>();
 		DeferredResult<ApiResponse<Void>> defResult = new DeferredResult<>();
 		mongoPostService.closePost(postId)
-		.doOnNext(post ->
-		{
-			
-			//result.setResponseObject(comment);
-			result.setStatus(HttpStatus.OK);
-			defResult.setResult(result);
-			
-		}
-				
+				.doOnNext(post ->
+						{
+
+							//result.setResponseObject(comment);
+							result.setStatus(HttpStatus.OK);
+							defResult.setResult(result);
+
+						}
+
 				)
-		.doOnError(error -> {
-			result.setStatus(HttpStatus.NOT_MODIFIED);
-			result.setMessage("Not able to close the Post " + error);
-			result.setDebugMessage(error.getCause().toString());
-			defResult.setResult(result);
-		}).subscribe();
-	
-		
+				.doOnError(error -> {
+					result.setStatus(HttpStatus.NOT_MODIFIED);
+					result.setMessage("Not able to close the Post " + error);
+					result.setDebugMessage(error.getCause().toString());
+					defResult.setResult(result);
+				}).subscribe();
+
 
 		return defResult;
-
 	}
-    
-//    @PostMapping("/post/{postId}/addComment")
-//    public DeferredResult<ApiResponse<Comment>> addComment(@PathVariable String postId, @RequestBody Comment commentBody)
-//
-//	{
-//		commentBody.setCommentId(UUID.randomUUID().toString());
-//		ApiResponse<Comment> result = new ApiResponse<Comment>();
-//		DeferredResult<ApiResponse<Comment>> defResult = new DeferredResult<>();
-//		commentRepository.insert(commentBody).doOnNext(comment -> {
-//			result.setResponseObject(comment);
-//			result.setStatus(HttpStatus.OK);
-//			defResult.setResult(result);
-//			mongoPostService.incrementCommentCount(postId);
-//			solrService.updateinSolr(postId);
-//		}).doOnError(error -> {
-//			result.setStatus(HttpStatus.NOT_MODIFIED);
-//			result.setMessage("Not ABle to Add the Comment " + error);
-//			result.setDebugMessage(error.getCause().toString());
-//			defResult.setResult(result);
-//		}).subscribe();
-//
-//		return defResult;
-//
-//	}
-    
-    @PostMapping("/post/{postId}/like/{userId}")
-    public DeferredResult<ApiResponse<Void>> addLike(@PathVariable String postId, @PathVariable String userId)
 
+	//    @PostMapping("/post/{postId}/addComment")
+	//    public DeferredResult<ApiResponse<Comment>> addComment(@PathVariable String postId, @RequestBody Comment commentBody)
+	//
+	//  {
+	//      commentBody.setCommentId(UUID.randomUUID().toString());
+	//      ApiResponse<Comment> result = new ApiResponse<Comment>();
+	//      DeferredResult<ApiResponse<Comment>> defResult = new DeferredResult<>();
+	//      commentRepository.insert(commentBody).doOnNext(comment -> {
+	//          result.setResponseObject(comment);
+	//          result.setStatus(HttpStatus.OK);
+	//          defResult.setResult(result);
+	//          mongoPostService.incrementCommentCount(postId);
+	//          solrService.updateinSolr(postId);
+	//      }).doOnError(error -> {
+	//          result.setStatus(HttpStatus.NOT_MODIFIED);
+	//          result.setMessage("Not ABle to Add the Comment " + error);
+	//          result.setDebugMessage(error.getCause().toString());
+	//          defResult.setResult(result);
+	//      }).subscribe();
+	//
+	//      return defResult;
+	//
+	//  }
+
+	@PostMapping("/post/{postId}/like/{userId}")
+	public DeferredResult<ApiResponse<Void>> addLike(@PathVariable String postId, @PathVariable String userId)
 	{
-		
+
 		ApiResponse<Void> result = new ApiResponse<Void>();
 		DeferredResult<ApiResponse<Void>> defResult = new DeferredResult<>();
-		
+
 		mongoPostService.incrementLikeCount(postId,userId)
-		.doOnNext(post -> {
-			//result.setResponseObject(post);
-			result.setStatus(HttpStatus.OK);
-			defResult.setResult(result);
-			solrService.updateinSolr(postId, userId);
-		}).doOnError(error -> {
+				.doOnNext(post -> {
+					//result.setResponseObject(post);
+					result.setStatus(HttpStatus.OK);
+					defResult.setResult(result);
+					solrService.updateinSolr(postId, userId);
+				}).doOnError(error -> {
 			result.setStatus(HttpStatus.NOT_MODIFIED);
 			result.setMessage("Not ABle to like the post " + error);
 			result.setDebugMessage(error.getCause().toString());
 			defResult.setResult(result);
 		}).subscribe();
-
 		return defResult;
-
 	}
-    
-    @PostMapping("/post/{postId}/unlike/{userId}")
-    public DeferredResult<ApiResponse<Void>> decreaseLike(@PathVariable String postId,@PathVariable String userId)
 
+	@PostMapping("/post/{postId}/unlike/{userId}")
+	public DeferredResult<ApiResponse<Void>> decreaseLike(@PathVariable String postId,@PathVariable String userId)
 	{
-		
+
 		ApiResponse<Void> result = new ApiResponse<Void>();
 		DeferredResult<ApiResponse<Void>> defResult = new DeferredResult<>();
-		
+
 		mongoPostService.decrementLikeCount(postId,userId)
-		.doOnNext(post -> {
-			//result.setResponseObject(post);
-			result.setStatus(HttpStatus.OK);
-			defResult.setResult(result);
-			solrService.updateinSolr(postId, userId);
-		}).doOnError(error -> {
+				.doOnNext(post -> {
+					//result.setResponseObject(post);
+					result.setStatus(HttpStatus.OK);
+					defResult.setResult(result);
+					solrService.updateinSolr(postId, userId);
+				}).doOnError(error -> {
 			result.setStatus(HttpStatus.NOT_MODIFIED);
 			result.setMessage("Not ABle to unlike the post " + error);
 			result.setDebugMessage(error.getCause().toString());
 			defResult.setResult(result);
 		}).subscribe();
-
 		return defResult;
-
 	}
-    
-    @PutMapping("/post/{postId}/follow/{userId}")
-    public DeferredResult<ApiResponse<Void>> addFollower(@PathVariable String postId, @PathVariable String userId)
 
+	@PutMapping("/post/{postId}/follow/{userId}")
+	public DeferredResult<ApiResponse<Void>> addFollower(@PathVariable String postId, @PathVariable String userId)
 	{
-		
+
 		ApiResponse<Void> result = new ApiResponse<>();
 		DeferredResult<ApiResponse<Void>> defResult = new DeferredResult<>();
-		
+
 		mongoPostService.addFollower(postId,userId)
-		.doOnNext(post -> {
-			//result.setResponseObject(post);
-			result.setStatus(HttpStatus.OK);
-			defResult.setResult(result);
-			solrService.updateinSolr(postId, userId);
-		}).doOnError(error -> {
+				.doOnNext(post -> {
+					//result.setResponseObject(post);
+					result.setStatus(HttpStatus.OK);
+					defResult.setResult(result);
+					solrService.updateinSolr(postId, userId);
+				}).doOnError(error -> {
 			result.setStatus(HttpStatus.NOT_MODIFIED);
 			result.setMessage("Not ABle to follow the post " + error);
 			result.setDebugMessage(error.getCause().toString());
 			defResult.setResult(result);
 		}).subscribe();
-
 		return defResult;
-
 	}
-    
-    @PutMapping("/post/{postId}/unfollow/{userId}")
-    public DeferredResult<ApiResponse<Void>> removeFollower(@PathVariable String postId, @PathVariable String userId)
 
+	@PutMapping("/post/{postId}/unfollow/{userId}")
+	public DeferredResult<ApiResponse<Void>> removeFollower(@PathVariable String postId, @PathVariable String userId)
 	{
-		
+
 		ApiResponse<Void> result = new ApiResponse<Void>();
 		DeferredResult<ApiResponse<Void>> defResult = new DeferredResult<>();
-		
+
 		mongoPostService.decrementFollower(postId, userId)
-		.doOnNext(post -> {
-		//	result.setResponseObject(post);
-			result.setStatus(HttpStatus.OK);
-			defResult.setResult(result);
-			solrService.updateinSolr(postId, userId);
-		}).doOnError(error -> {
+				.doOnNext(post -> {
+					//  result.setResponseObject(post);
+					result.setStatus(HttpStatus.OK);
+					defResult.setResult(result);
+					solrService.updateinSolr(postId, userId);
+				}).doOnError(error -> {
 			result.setStatus(HttpStatus.NOT_MODIFIED);
 			result.setMessage("Not ABle to unfollow the post " + error);
 			result.setDebugMessage(error.getCause().toString());
 			defResult.setResult(result);
 		}).subscribe();
-
 		return defResult;
-
 	}
-    
 
+	//    @GetMapping("/post/findallSeekingPosts/{userId}")
+	//    public Flux<Post> getSeekingPosts(@PathVariable String userId)
+	//
+	//    {ApiResponse<Post> result = new ApiResponse<Post>();
+	//  DeferredResult<ApiResponse<Post>> defResult = new DeferredResult<>();
+	//  postRepository.getAllSeekingPosts(userId).flatMap(post -> {
+	//      result.setStatus(HttpStatus.OK);
+	//      result.setResponseObject(post);
+	//      defResult.setResult(result);
+	//  }
+	//
+	//  ).doOnError(error -> {
+	//      result.setStatus(HttpStatus.NOT_MODIFIED);
+	//      result.setMessage("Not ABle to Add the user " + error);
+	//      result.setDebugMessage(error.getCause().toString());
+	//      defResult.setResult(result);
+	//  }).subscribe();
+	//
+	//  return defResult;}
 
+	@GetMapping("/post/location/{loc}")
+	public Flux<Post> getPostByLocation(@PathVariable String loc)
+	{
+		return postRepository.getPostByLocation(loc);
+	}
 
-
-
-
-
-
-
-
-
-
-//    @GetMapping("/post/findallSeekingPosts/{userId}")
-//    public Flux<Post> getSeekingPosts(@PathVariable String userId)
-//    
-//    {ApiResponse<Post> result = new ApiResponse<Post>();
-//	DeferredResult<ApiResponse<Post>> defResult = new DeferredResult<>();
-//	postRepository.getAllSeekingPosts(userId).flatMap(post -> {
-//		result.setStatus(HttpStatus.OK);
-//		result.setResponseObject(post);
-//		defResult.setResult(result);
-//	}
-//
-//	).doOnError(error -> {
-//		result.setStatus(HttpStatus.NOT_MODIFIED);
-//		result.setMessage("Not ABle to Add the user " + error);
-//		result.setDebugMessage(error.getCause().toString());
-//		defResult.setResult(result);
-//	}).subscribe();
-//
-//	return defResult;}
-
-    
-
-    @GetMapping("/post/location/{loc}")
-    public Flux<Post> getPostByLocation(@PathVariable String loc)
-    {
-        return postRepository.getPostByLocation(loc);
-    }
-    
-    @DeleteMapping("/deleteAll")
-    public void deleteAll()
-    {
-        postRepository.deleteAll()
-        .doOnError(error -> System.out.println("failed   "+ error))
-        .doOnNext(onNext->System.out.println("succedd "+ onNext))
-        .subscribe(System.out :: println);
-    }
-
-
-
-
+	@DeleteMapping("/deleteAll")
+	public void deleteAll()
+	{
+		postRepository.deleteAll()
+				.doOnError(error -> System.out.println("failed   "+ error))
+				.doOnNext(onNext->System.out.println("succedd "+ onNext))
+				.subscribe(System.out :: println);
+	}
 }
--- src/main/java/com/yolo/PostManagementService/Services/MongoPostService.java
+++ src/main/java/com/yolo/PostManagementService/Services/MongoPostService.java
@@ -1,47 +1,34 @@
 package com.yolo.PostManagementService.Services;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
-
 import com.yolo.PostManagementService.enums.PostStatus;
 import com.yolo.PostManagementService.repositories.api.PostRepository;
 import com.yolo.PostManagementService.resources.Post;
-
 import reactor.core.publisher.Mono;
-
 @Service
 public class MongoPostService {
-
 	@Autowired
 	PostRepository postRepo;
-	
+
 	@Autowired
 	MongoTemplate mongoTemplate;
-
 	public void incrementCommentCount(String postId) {
 		// TODO Auto-generated method stub
-
 		postRepo.findById(postId).doOnNext(post -> {
-			if (null != post.getCommentsCounts())
-				post.setCommentsCounts(post.getCommentsCounts() + 1);
-			else
-				post.setCommentsCounts(1);
-
-			postRepo.save(post).retry().subscribe();
-		}
-
+					if (null != post.getCommentsCounts())
+						post.setCommentsCounts(post.getCommentsCounts() + 1);
+					else
+						post.setCommentsCounts(1);
+					postRepo.save(post).retry().subscribe();
+				}
 		).subscribe();
-
 	}
-
 	public Mono<Post> incrementLikeCount(String postId, String userId) {
 		// TODO Auto-generated method stub
 		postRepo.findById(postId).doOnNext(post ->
-
 		{
 			if (null != post.getLikeCounts()) {
 				if (!post.getLikerIds().contains(userId)) {
@@ -57,19 +44,13 @@ public class MongoPostService {
 				post.setLikerIds(likers);
 			}
 			postRepo.save(post).retry().subscribe();
-
 		}).subscribe();
-
 		Mono<Post> updatedPost = postRepo.findById(postId);
-
 		return updatedPost;
 	}
-
 	public Mono<Post> decrementLikeCount(String postId, String userId) {
 		// TODO Auto-generated method stub
-
 		postRepo.findById(postId).doOnNext(post -> {
-
 			Integer count = post.getLikeCounts();
 			if (count >= 0 && post.getLikerIds().contains(userId)) {
 				post.setLikeCounts(--count);
@@ -78,66 +59,53 @@ public class MongoPostService {
 			likers.remove(userId);
 			post.setLikerIds(likers);
 			postRepo.save(post).retry().subscribe();
-
 		}).subscribe();
-
 		Mono<Post> updatedPost = postRepo.findById(postId);
 		return updatedPost;
 	}
-
 	public Mono<Post> addFollower(String postId, String userId) {
 		// TODO Auto-generated method stub
-
 		postRepo.findById(postId).doOnNext(post -> {
-			if (null != post.getPostFollowers()) {
-				if (!post.getPostFollowers().contains(userId)) {
-					List<String> followers = post.getPostFollowers();
-					followers.add(userId);
-					post.setPostFollowers(followers);
+					if (null != post.getPostFollowers()) {
+						if (!post.getPostFollowers().contains(userId)) {
+							List<String> followers = post.getPostFollowers();
+							followers.add(userId);
+							post.setPostFollowers(followers);
+						}
+					} else {
+						List<String> followers = new ArrayList<String>();
+						followers.add(userId);
+						post.setPostFollowers(followers);
+					}
+
+					postRepo.save(post).retry().subscribe();
 				}
-			} else {
-
-				List<String> followers = new ArrayList<String>();
-				followers.add(userId);
-				post.setPostFollowers(followers);
-			}
-			
-			postRepo.save(post).retry().subscribe();
-
-		}
-
 		).subscribe();
 		Mono<Post> postMono = postRepo.findById(postId);
 		return postMono;
 	}
-
 	public Mono<Post> decrementFollower(String postId, String userId) {
 		// TODO Auto-generated method stub
-
 		postRepo.findById(postId).doOnNext(post -> {
-			List<String> followers = post.getPostFollowers();
-			followers.remove(userId);
-			post.setLikerIds(followers);
-			postRepo.save(post).retry().subscribe();
-		}
-
+					List<String> followers = post.getPostFollowers();
+					followers.remove(userId);
+					post.setLikerIds(followers);
+					postRepo.save(post).retry().subscribe();
+				}
 		).subscribe();
 		Mono<Post> postMono = postRepo.findById(postId);
 		return postMono;
 	}
-	
-	
+
+
 	public Mono<Post> closePost(String postId) {
 		// TODO Auto-generated method stub
-
 		postRepo.findById(postId).doOnNext(post -> {
-			post.setStatus(PostStatus.CLOSED);
-			postRepo.save(post).retry().subscribe();
-		}
-
+					post.setStatus(PostStatus.CLOSED);
+					postRepo.save(post).retry().subscribe();
+				}
 		).subscribe();
 		Mono<Post> postMono = postRepo.findById(postId);
 		return postMono;
 	}
-
 }
